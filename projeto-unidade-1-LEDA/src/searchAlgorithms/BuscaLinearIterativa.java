fadfd31ee6e8e737ff804968e5b6baabeacffe4d
João Paulo de Araujo <jpauloo.araujo@gmail.com>
2025-09-30 01:39:43 -0300

    Adiciona algoritmos de busca e classe de análise de performance

--- projeto-unidade-1-LEDA/src/searchAlgorithms/BuscaLinearIterativa.java
+++ projeto-unidade-1-LEDA/src/searchAlgorithms/BuscaLinearIterativa.java
@@ -8,22 +8,22 @@ public class BuscaLinearIterativa implements ISearchingAlgorithm {
 	@Override
 	public boolean search(Estudante[] dados, Estudante estudante) {
 		int n = dados.length;
-		for(int i = 0; i < n; i++) {
-			if(dados[i].compareTo(estudante) == 0) {
+		for (int i = 0; i < n; i++) {
+			if (dados[i].compareTo(estudante) == 0) {
 				return true;
 			}
-		}		
+		}
 		return false;
 	}
 
 	@Override
 	public boolean search(int[] array, int numero) {
 		int n = array.length;
-		for(int i = 0; i < n; i++) {
-			if(array[i] == numero) {
+		for (int i = 0; i < n; i++) {
+			if (array[i] == numero) {
 				return true;
 			}
 		}
-		return false; 
+		return false;
 	}
 }
